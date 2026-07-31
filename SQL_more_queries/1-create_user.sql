bcba1aae8807c36b6799f6cb45a36d4c6554acb9
Ntwali O. Axel <132359348+execxel@users.noreply.github.com>
2026-07-31 23:37:51 +0200

    Change

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,5 @@
 -- 1. Root user
 -- Create user user_0d_1 with all privileges and set its password.
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+ALTER USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
