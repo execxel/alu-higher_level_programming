53be82a4f60161ede0193877fcf761b345be0b98
Ntwali O. Axel <132359348+execxel@users.noreply.github.com>
2026-07-31 22:32:04 +0200

    Fix

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
-"""Function that checks if object inherits from specified class."""
+"""
+Module: 4-inherits_from
+Description: Function that checks if object inherits from the
+specified class.
+"""
 
 
 def inherits_from(obj, a_class):
-    """Return True if obj inherits from a_class (direct or indirect), else False."""
+    """Return True if obj inherits from a_class (direct or indirect).
+
+    Returns False if obj is exactly an instance of `a_class`.
+    """
     return isinstance(obj, a_class) and type(obj) is not a_class
