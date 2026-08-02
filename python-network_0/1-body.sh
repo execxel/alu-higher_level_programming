592a69487a4ade46e83e2be90d3536fdbe342c98
Ntwali O. Axel <132359348+execxel@users.noreply.github.com>
2026-08-02 15:24:00 +0200

    Mod

--- python-network_0/1-body.sh
+++ python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Sends a GET request to a URL and displays the body only if the status code is 200
-response=$(curl -s -w "\n%{http_code}" "$1"); code=$(echo "$response" | tail -n1); [ "$code" -eq 200 ] && echo "$response" | sed '$d'
+# Displays the body of the response only for a 200 status code
+curl -sL -f "$1"
