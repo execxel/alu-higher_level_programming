4438e354b752c909cff3ff180c86954ec0954e5a
Ntwali O. Axel <132359348+execxel@users.noreply.github.com>
2026-08-02 15:47:29 +0200

    Fix

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Displays the size of the body of the response in bytes
-curl -s "$1" | wc -c
+# Sends a request to the URL in the first argument and displays the size of the response body in bytes
+curl -s -o /dev/null -w "%{size_download}" "$1"
