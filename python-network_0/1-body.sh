1840408527dfc3756db254a908382255562583b2
Ntwali O. Axel <ntwaliaxelomega@gmail.com>
2026-08-02 16:27:42 +0200

    Fix

--- python-network_0/1-body.sh
+++ python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Sends a GET request to a URL and displays the body only if the status code is 200
-response=$(curl -s -w "\n%{http_code}" "$1"); code=$(echo "$response" | tail -n1); [ "$code" -eq 200 ] && echo "$response" | sed '$d'
+# Sends a GET request to the URL and displays the response body only if the status code is 200
+[ "$(curl -s -o /dev/null -w "%{http_code}" "$1")" = "200" ] && curl -s "$1"
